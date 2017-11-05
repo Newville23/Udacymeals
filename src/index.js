384af4c711b32847481e731aa7d7fec5c748f793
David Villanueva <DavidVillanueva@David-Villanueva-MacBook-Pro.local>
2017-11-05 14:47:48 -0500

    feat: Actions, reducers and store

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+console.log(store.getState())
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
