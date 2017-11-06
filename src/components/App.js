db3a7ddc3fc82d0eb3680aeb5e53a2a28f71bed1
David Villanueva <DavidVillanueva@David-Villanueva-MacBook-Pro.local>
2017-11-06 17:01:03 -0500

    feat: Redux Provider Component

--- src/components/App.js
+++ src/components/App.js
@@ -5,40 +5,31 @@ import { addRecipe } from '../actions'
 
 
 class App extends Component {
-  state = {
-    calendar: null
-  } 
-  componentDidMount () {
-    const { store } = this.props
-    store.subscribe(() => {
-      this.setState(() => ({
-        calendar: store.getState()
-      }))
-    })
-   }
-  submitFood = () => {
-    this.props.store.dispatch(addRecipe({
-      day: 'monday',
-      meal: 'breakfast',
-      recipe: {
-        label: this.input.value
-      }
-    }))
-    this.input.value = ''
-   }
+  // state = {
+  //   calendar: null
+  // } 
+  // componentDidMount () {
+  //   const { store } = this.props
+  //   store.subscribe(() => {
+  //     this.setState(() => ({
+  //       calendar: store.getState()
+  //     }))
+  //   })
+  //  }
+  // submitFood = () => {
+  //   this.props.store.dispatch(addRecipe({
+  //     day: 'monday',
+  //     meal: 'breakfast',
+  //     recipe: {
+  //       label: this.input.value
+  //     }
+  //   }))
+  //   this.input.value = ''
+  //  }
   render() {
     return (
       <div className="App">
-        <input
-          type='text'
-          ref={(input) => this.input = input}
-          placeholder="Monday's Breakfast"
-        />
-        <button onClick={this.submitFood}>Submit</button>
-
-        <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
+        Hallo!!
       </div>
     );
   }
